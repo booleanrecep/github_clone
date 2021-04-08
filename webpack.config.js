8c492c07467fbdcf42be26a3a586ea2eca1368ae
Recep Öztürk <recep.fed@gmail.com>
2021-04-08 14:39:18 +0300

    some webpack tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,79 @@
-const path=require("path")
-const HTMLWebpackPlugiin=require("html-webpack-plugin")
-
-module.exports={
-    // target:"web",
-    entry:"./src/index.js",
-    output:{
-        path:path.join(__dirname,"build"),
-        filename:"bundle.js"
-    },
-    module:{
-        rules:[
-        {
-            test:/\.(js|jsx)$/,
-            exclude:/node_modules/,
-            use:{
-                loader:"babel-loader"
-            }
+const path = require("path");
+const HTMLWebpackPlugiin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+  // target:"web",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        {
-            test:/\.html$/,
-            use:[
-                {
-                    loader:"html-loader"
-                }
-            ]
-        },
-        {
-            test: /\.(s[ac]ss)$/i,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ],
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
           },
+        ],
+      },
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                indentWidth: 4,
+                includePaths: ["absolute/path/a", "absolute/path/b"],
+                outputStyle: "compressed",
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name][hash].[ext]",
+            },
+          },
+        ],
+      },
     ],
-    },
-    
-    plugins:[
-        new HTMLWebpackPlugiin({
-            template:"./public/index.html",
-            filename:"./index.html"
-        })
-    ],
-    // devtool:"cheap-module-eval-source-map",
-    // devtool:"cheap-module-source-map",
-    devServer:{
-        contentBase:path.join(__dirname,"public")
-    }
+  },
 
-    
-}
+  plugins: [
+    new HTMLWebpackPlugiin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+
+  // devtool:"cheap-module-source-map",
+
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+};
